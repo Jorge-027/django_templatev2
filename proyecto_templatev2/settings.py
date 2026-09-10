f762dece20d567a41d68a4a27f15635c96eaf1d7
Jorge-027 <jorge.sepulveda76@inacapmail.cl>
2026-09-10 12:19:23 -0300

    proyecto base inicializado con entorno virtual y requerminietos2

--- proyecto_templatev2/settings.py
+++ proyecto_templatev2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$)3f91lw-&5(e(l76_-5oi5$q&#lnhhls(6z&r+c8i)(n_4ne*'
+SECRET_KEY = 'django-insecure-73io@v@sg&p=&@&4jl0bj6l!g#fdxjy*&lcpdmw6gk@=_rhp_o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
