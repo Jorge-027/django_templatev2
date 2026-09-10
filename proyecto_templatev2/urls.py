d8de5e000281b84cf225e529316184c200e6f20b
Jorge-027 <jorge.sepulveda76@inacapmail.cl>
2026-09-10 12:44:31 -0300

    app1 creada e integrada al base.html

--- proyecto_templatev2/urls.py
+++ proyecto_templatev2/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('app1/', include('app1.urls')),
     path('', include('inicio.urls')),
     path('admin/', admin.site.urls),
 ]
